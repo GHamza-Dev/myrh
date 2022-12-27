74c3e8b99e974288fe167527edd873ed6c01e04a
Ghamza-dev <hamza.gassai@gmail.com>
2022-12-27 14:12:25 +0100

    Initial commit

---
+++ src/test/java/flat/io/myrh/MyrhApplicationTests.java
@@ -0,0 +1,13 @@
+package flat.io.myrh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyrhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
