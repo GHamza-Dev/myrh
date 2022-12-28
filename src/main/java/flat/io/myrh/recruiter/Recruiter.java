ca94a0f2726c30065174cdf5e3ff61209dc9897a
Ghamza-dev <hamza.gassai@gmail.com>
2022-12-28 16:35:05 +0100

    Add deafault constructor

--- src/main/java/flat/io/myrh/recruiter/Recruiter.java
+++ src/main/java/flat/io/myrh/recruiter/Recruiter.java
@@ -3,9 +3,17 @@ package flat.io.myrh.recruiter;
 import flat.io.myrh.user.AppUser;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Entity
+@Data
 public class Recruiter extends AppUser {
     @Column(length = 55)
     private String companyName;
+
+    public Recruiter() {
+    }
 }
